03147a680878048b76e5392f2d8f3905a66f7624
sandeepchowdary367 <66997198+sandeepchowdary367@users.noreply.github.com>
2020-06-19 09:51:42 +0530

    Update SI.py

--- SI.py
+++ SI.py
@@ -1,5 +1,5 @@
-p= float(input("enter the principal:"))
-t= float(input("enter the number of years:"))
-r= float(input("enter the rate of intrest:"))
+p=float(input("enter the principal:"))
+t=float(input("enter the number of years:"))
+r=float(input("enter the rate of intrest:"))
 SI=(p*t*r)/100
 print(SI)
